360fde6974d58ce92425db54acf410e4cb2a05f3
Jean-Yves Tinevez <tinevez@pasteur.fr>
2025-11-25 15:18:01 +0000

    Inherit from CondaCLIConfigurator
    CondaExecutableCLIConfigurator is removed as it duplicates its
    mother class features.

--- src/main/java/fiji/plugin/trackmate/yolo/YOLOCLI.java
+++ src/main/java/fiji/plugin/trackmate/yolo/YOLOCLI.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -32,10 +32,10 @@ import static fiji.plugin.trackmate.yolo.YOLODetectorFactory.KEY_YOLO_MODEL_FILE
 
 import java.util.Collections;
 
-import fiji.plugin.trackmate.util.cli.CondaExecutableCLIConfigurator;
+import fiji.plugin.trackmate.util.cli.CondaCLIConfigurator;
 import ij.IJ;
 
-public class YOLOCLI extends CondaExecutableCLIConfigurator
+public class YOLOCLI extends CondaCLIConfigurator
 {
 
 	private final PathArgument modelPath;
